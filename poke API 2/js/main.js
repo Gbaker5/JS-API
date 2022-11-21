971b99b943c93cbaea0c5e7f9de2095b15fbc350
Gjarred Baker <gjarred23@gmail.com>
2022-11-21 15:33:32 -0600

    js for getfetch finished

--- poke API 2/js/main.js
+++ poke API 2/js/main.js
@@ -6,6 +6,14 @@ let image = document.querySelector('image')
 
 
 function getFetch(){
+  //clear
+  document.querySelector('#pokename').innerText = '' 
+  document.querySelector('#pokeAbility').innerText = ''
+  document.querySelector('#pokeMoves').innerText = ''
+  document.querySelector('#pokeTypes').innerText = ''
+  document.querySelector('#wgt').innerText = ''
+
+  
   const choice = document.querySelector('input').value //value from input
   const url = 'https://pokeapi.co/api/v2/pokemon/'+choice 
 
@@ -20,14 +28,12 @@ function getFetch(){
         
         //abilities
         data.abilities.forEach(obj => {
-          console.log(obj.ability.name)
           const abilityLi = document.createElement('li') //create an li
           abilityLi.textContent = obj.ability.name //add text to li
           document.querySelector('#pokeAbility').appendChild(abilityLi)//append the li to the ul
         })
         
         //moves
-        console.log(data.moves)
         data.moves.forEach(obj => {
           const moveLi = document.createElement('li') //create an li
           moveLi.textContent = obj.move.name //add text to li
@@ -35,7 +41,6 @@ function getFetch(){
         })
         
         //types
-        console.log(data.types)
         data.types.forEach(obj => {
           const typeLi = document.createElement('li') //create an li
           typeLi.textContent = obj.type.name //add text to li
@@ -46,16 +51,28 @@ function getFetch(){
         document.querySelector('#wgt').innerText = data.weight
 
         //stats
+        console.log(data.stats)
+        console.log(data.stats[1].stat.name)
+        data.stats.forEach( obj => {
+          
+          console.log(obj.stat.name)
+          //const statP = document.createElement('p')//create paragraph
+          //statP.id = 'statPara'//set id of newly created pragraph to 'statPara'
+          //statP.textContent = obj.stat.name //add text to paragraph
+          
+          //document.querySelector('#statPara').appendChild(statP)
+        })
 
-        
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
+document.querySelector('#test').addEventListener('click', getFetch2)
+
 function getFetch2(){
-  const url = document.querySelectorAll('span').innerText
+  const url = 'https://pokeapi.co/api/v2/stat/2/'
 
 
   fetch(url)
